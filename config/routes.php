be4d3cba06eac74be96f8bab841188d3419b6a3b
Daniele Conca <d.conca@metarete.it>
2025-03-21 15:41:56 +0100

    Expression "$routes" on a separate line does not do anything.
    phpstan level 4

--- config/routes.php
+++ config/routes.php
@@ -1,15 +1,10 @@
 <?php
 
-use Metarete\ComuniBundle\Controller\HelloController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 /**
  * @link https://symfony.com/doc/current/bundles/best_practices.html#routing
  */
 return static function (RoutingConfigurator $routes): void {
-    $routes
-        // ->add('metarete_comuni_hello_controller', 'test')
-        //     ->controller(HelloController::class)
-        //     ->methods(['GET'])
-    ;
+    // nothing to do
 };
